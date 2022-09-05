f7edba0379453a9e30bbe61160dc046c2b4bd46f
genius <geniusremix@naver.com>
2022-09-05 17:19:21 +0900

    first commit from genius

---
+++ src/main/java/com/NAPHAL/NAPHAL_dev/NaphalDevApplication.java
@@ -0,0 +1,13 @@
+package com.NAPHAL.NAPHAL_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaphalDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaphalDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NAPHAL/NAPHAL_dev/NaphalDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NAPHAL.NAPHAL_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaphalDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
